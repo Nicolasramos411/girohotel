977dee427e5a4146c2b497fe682210704960b630
Nicolasramos411 <nicolasramos41@uc.cl>
2022-07-08 21:49:02 -0400

    cambios a la página web

--- src/static/rooms.js
+++ src/static/rooms.js
@@ -3,7 +3,7 @@ const rooms = [
     description: "The Great Room is a room that is designed to be a place for you to relax and enjoy the outdoors. It is a great place to spend your time and have fun.",
     image: process.env.PUBLIC_URL + '/assets/room-1.jpg'
   },
-  { title: "Deluxe Room",
+  { title: "Premium Room",
     description: "The Great Room is a room that is designed to be a place for you to relax and enjoy the outdoors. It is a great place to spend your time and have fun.",
     image: process.env.PUBLIC_URL + '/assets/room-2.jpg'
   },
